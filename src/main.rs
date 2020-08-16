401e21328625bd8c523cb74f1771dc51426b0e25
Clark Everetts <clark.everetts@gmail.com>
2020-08-16 18:13:35 -0500

    Variables, data types, conditionals, control structures

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
 fn main() {
-    another_function(5, 6);
-    let x = five();
-    println!("x: {}", x);
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    } else if number > 5 {
+        println!("condition was false");
+    } else {
+        println!("both conditions were false");
+    }
 
-    let x = plus_one(3);
-    println!("x from plus_one: {}", x);
-}
+    let condition = true;
+    let number_two = if condition { 5 } else { 6 };
+    println!("number_two is {}", number_two);
 
-fn another_function(x: i32, y: i32) {
-    println!("x = {}, and y = {}", x, y);
-}
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
 
-fn five() -> i32 {
-    5
-}
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
 
-fn plus_one(x: i32) -> i32 {
-    x + 1
+    println!("result is {}", result);
+
+    let mut number = 10;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("Liftoff!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("val: {}", a[index]);
+        index += 1;
+    }
+
+    for element in a.iter() {
+        println!("val: {}", element);
+    }
+
+    for number in (1..6).rev() {
+        println!("{}!", number);
+    }
+    println!("Liftoff!");
 }
