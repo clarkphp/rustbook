cb08e95ae200c48b2b0ab9e113ec7d57f93984f0
Clark Everetts <clark.everetts@gmail.com>
2020-08-16 17:23:08 -0500

    Intro to functions; remove rand dependency

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("The value of x is {}", x);
+    another_function(5, 6);
+    let x = five();
+    println!("x: {}", x);
 
-    let sum = 5 + 10;
-    let diff = 95.5 - 4.3;
-    let product = 4 * 30;
-    let quotient = 56.7 / 32.2;
-    let remainder = 43 % 5;
+    let x = plus_one(3);
+    println!("x from plus_one: {}", x);
+}
 
-    let t = true;
-    let f: bool = false;
+fn another_function(x: i32, y: i32) {
+    println!("x = {}, and y = {}", x, y);
+}
 
-    let c = 'z';
-    let z = 'Z';
-    let heart_eyed_cat = '😻';
+fn five() -> i32 {
+    5
+}
 
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("The value of y is: {}", y);
-
-    let five_hundred = tup.0;
-    let six_point_four = tup.1;
-    let one = tup.2;
-
-    let a = [1, 2, 3, 4, 5];
-    let ar: [i32; 5] = [1, 2, 3, 4, 5];
-    let arr = [3; 5];
-
-    let index = 10;
-    let element = a[index]; // out of bounds array access
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
