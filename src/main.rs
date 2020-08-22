79248fa0e8dc9de6d986fa54f140d955f56161fb
Clark Everetts <clark.everetts@gmail.com>
2020-08-22 09:59:10 -0500

    Demonstrate a need for string slices

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,26 @@
 fn main() {
-    let mut s = String::from("A new test string");
-    println!("s is {}", s);
+    let mut test_string = String::from("This is a test string.");
+    let word_end_index = first_word(&test_string);
+    println!(
+        "Input string: '{}'\nWord 1: {}",
+        test_string, word_end_index
+    );
+    test_string.clear(); // index to end of word is now invalid!
 
-    let str_len = calc_length(&s);
-    println!("length of '{}' is {}", s, str_len);
-
-    change(&mut s);
-    println!("s is {}", s);
-
-    let str_len = calc_length(&s);
-    println!("length of '{}' is {}", s, str_len);
-
-    let ref_1 = &s;
-    let ref_2 = &s;
-    println!("{}, {}", ref_1, ref_2);
-
-    let ref_3 = &mut s;
-    println!("{}", ref_3);
-
-    let not_a_ref_to_nothing = no_dangle();
-    println!("{}", not_a_ref_to_nothing);
+    println!(
+        "Input string: '{}'\nWord 1: {}",
+        test_string, word_end_index
+    );
 }
 
-fn calc_length(input: &String) -> usize {
-    // argument is a reference; function now owns the address of input
-    input.len() // move return value to caller
-}
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
 
-fn change(some_string: &mut String) {
-    // reference is dropped, not the data it points to
-    some_string.push_str(", with appended text");
-}
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
 
-fn no_dangle() -> String {
-    // scope of s is this function only
-    let s = String::from("Rust prevents dangling references!");
-
-    // return the string, not a ref to it; offers ownership to the caller
-    s
+    s.len()
 }
